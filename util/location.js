76fa84726308960022a758792871f0bfb168ee34
Michael Irby <michael.irbstoomuch@gmail.com>
2020-12-17 10:46:48 -0500

    finish up api

---
+++ util/location.js
@@ -0,0 +1,6 @@
+// Dummy util function instead of using real google API
+const getCoordsForAddress = (address) => {
+  return { lat: 40.7484474, lng: -73.9871516 };
+};
+
+module.exports = getCoordsForAddress;
